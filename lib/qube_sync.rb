d35bae1d02d026ee32f240790e6bc2ebcb62df3f
Garrett Lancaster <glancast@garrettlancaster.com>
2025-01-31 11:21:32 -0800

    initial commit

---
+++ lib/qube_sync.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "qube_sync/version"
+
+module QubeSync
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/qube_sync_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe QubeSync do
+  it "has a version number" do
+    expect(QubeSync::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
